e19881856d0e82d15a2bb8c529d0fdb6e34d7d42
Matheus de Azevedo <matheus.marpereira@gmail.com>
2024-05-26 19:16:16 -0300

    More stuff 2

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -1,7 +1,7 @@
-vim.cmd("set number relativenumber")
+vim.cmd('set number relativenumber')
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd('set expandtab')
+vim.cmd('set tabstop=4')
+vim.cmd('set shiftwidth=4')
 
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
